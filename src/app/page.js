64d5482f55555f0608f87b9693cd55792f665f82
theekharbanda <namankharbanda512@gmail.com>
2025-05-02 22:09:46 +0530

    Initial Commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,162 @@
-import Image from "next/image";
+
+"use client"
+import { useEffect, useMemo, useState } from 'react';
+
+const FILTER_CONFIG = {
+  experiences: ["0-5", "5-10", "10-15", "15-20"],
+  fees: ["100-500", "500-1000", "1000+"],
+  language: ["Hindi", "English", "Punjabi", "Marathi", "Tamil"],
+};
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [doctors,setDoctors] = useState([]);
+  const [filter, setfilter] = useState({
+    experiences: [],
+    fees: [],
+    languages: []
+  });
+
+  useEffect(()=>{
+    const fetchDoctors= async()=>{
+      try{
+        const res = await fetch('http://localhost:8080/api/doctors', {
+          method: "POST",
+          headers:{
+            "Content-Type":"application/json"
+          },
+          body:JSON.stringify(filter)
+        });
+        if (!res.ok) throw new Error("Failed to fetch data.");
+        const data = await res.json();
+        setDoctors(data); 
+      }catch(error){
+        console.log(res);
+        console.log("Error ....");
+      }
+    }
+    fetchDoctors();
+  },[filter])
+  
+
+  
+
+
+  const handleFilter = (category, value) => {
+    setfilter(prev => {
+      const updatedCategory = prev[category].includes(value)
+        ? prev[category].filter(item => item !== value)
+        : [...prev[category], value];
+
+      return {
+        ...prev,
+        [category]: updatedCategory
+      }
+    });
+  }
+
+
+  return (
+    <div className="h-screen w-full">
+      <div className="w-full h-full grid grid-cols-12 border">
+        <div className="col-span-3 flex justify-center items-center h-full ">
+          <div className="flex flex-col gap-5 p-5">
+            <div className="filter1">
+              <div className="text-xl font-bold mb-1">
+                Experience
+              </div>
+              <div className="flex flex-col gap-2">
+                {FILTER_CONFIG.experiences.map((range, idx) => (
+                  <label key={idx} className="flex items-center gap-2 cursor-pointer">
+                    <input
+                      type="checkbox"
+                      className="w-4 h-4"
+                      checked={filter.experiences.includes(range)}
+                      onChange={() => handleFilter('experiences', range)}
+                    />
+                    <span className="text-sm">{range} years</span>
+                  </label>
+                ))}
+              </div>
+            </div>
+
+            <div className="filter2">
+              <div className="text-xl font-bold mb-1">
+                Fees
+              </div>
+              <div className="flex flex-col gap-2">
+                {FILTER_CONFIG.fees.map((range, idx) => (
+                  <label key={idx} className="flex items-center gap-2 cursor-pointer">
+                    <input
+                      type="checkbox"
+                      className="w-4 h-4"
+                      checked={filter.fees.includes(range)}
+                      onChange={() => handleFilter('fees', range)}
+                    />
+                    <span className="text-sm">{range}</span>
+                  </label>
+                ))}
+              </div>
+            </div>
+
+            <div className="filter3">
+              <div className="text-xl font-bold mb-1">
+                Langauge
+              </div>
+              <div className="flex flex-col gap-2">
+                {FILTER_CONFIG.language.map((range, idx) => (
+                  <label key={idx} className="flex items-center gap-2 cursor-pointer">
+                    <input
+                      type="checkbox"
+                      className="w-4 h-4"
+                      checked={filter.languages.includes(range)}
+                      onChange={() => handleFilter('languages', range)}
+                    />
+                    <span className="text-sm">{range}</span>
+                  </label>
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="col-span-9 flex flex-col overflow-hidden ">
+          <div className="h-20 ">
+            <div className="text-xl font-semibold">
+              Consult General Physicians Online - Internal Medicine Specialists
+            </div>
+          </div>
+          <div className="flex-1 overflow-y-auto ">
+            <div className="flex flex-col gap-3">
+              {doctors?.length === 0 && (
+                <div className="text-center text-gray-500 mt-4">No doctors match the selected filters.</div>
+              )}
+              {doctors?.map(doctor => (
+                <div key={doctor.id} className="w-200 border rounded-xl h-30 overflow-hidden shadow-md hover:shadow-lg transition-all">
+                  <div className="grid grid-cols-12 h-full">
+                    <div className="col-span-3 flex justify-center items-center  h-full">
+                      <div className="w-20 h-20 bg-gray-300 rounded-full"></div>
+                    </div>
+                    <div className="col-span-6 flex flex-col justify-center px-4 gap-1">
+                      <h2 className="text-lg font-semibold">{doctor.name}</h2>
+                      <p className="text-gray-500 text-sm">{doctor?.specialization} | {doctor.experience} years Experience | {doctor.languages.join(",")} | {doctor.fees}</p>
+                    </div>
+                    <div className="col-span-3 flex justify-center items-center px-2">
+                      <button className="w-40 bg-transparent outline px-4 py-2 rounded hover:cursor-pointer">
+                        Consult Online
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+
+
+
+        </div>
+      </div>
     </div>
   );
 }
